31f4f07d20660dcda38a01db03309f4174ebd14b
Artemiy Vasilchenko <teme4@mail.ru>
2024-10-21 16:39:12 +0300

    тестирование USB

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 ﻿#include <stm32f1xx.h>
 #include "gpio.hpp"
 
-// Set SYSTEM CLOCK = 72Mhz. AHB CLOCK = 72Mhz. APB1 CLOCK = 36Mhz. APB2 CLOCK = 72Mhz. USB CLOCK = 48Mhz.
-// (Alternatively use my function "ClockInit()" in the example code, you will find it's definition in "RCC_CLOCK.h",
-//  in folder Core/Inc).
+// макрос адресации
+#define REG(x) (*((volatile unsigned int)(x)))
+
+// базовый адрес регистров драйвера
+#define USB_BASE_ADDR 0x40005C00
+// адрес начала области памяти драйвера USB
+#define USB_PMA_ADDR 0x40006000
+
+// регистры состояния конечных точек
+#define EP0R REG(USB_BASE_ADDR)
+#define EP1R REG(0x40005C04)
+#define EP2R REG(0x40005C08)
+#define EP3R REG(0x40005C0C)
+#define EP4R REG(0x40005C10)
+#define EP5R REG(0x40005C14)
+#define EP6R REG(0x40005C18)
+#define EP7R REG(0x40005C1C)
+#define ENDPOINT(bEpNum) REG(USB_BASE_ADDR + (bEpNum) * 4)
+#define PMA_BUF(INum) REG(USB_PMA_ADDR + (INum)4)
+#define PMA_SBUF(SINum) (((volatile unsigned short int *)(USB_PMA_ADDR + (SINum) * 2)))
+
+// остальные регистры
+#define CNTR REG(USB_BASE_ADDR + 0x40)
+#define ISTR REG(USB_BASE_ADDR + 0x44)
+#define DADDR REG(USB_BASE_ADDR + 0x4C)
+#define BTABLE REG(USB_BASE_ADDR + 0x50)
+
+#define HSE_STARTUP_TIMEOUT ((uint32_t)100) /*!< Time out for HSE start up, in ms */
+#define RCC_CFGR_PLLSRC_HSE ((uint32_t)0x00010000)
+#define VECT_TAB_OFFSET 0x4000U /*!< Vector Table base offset field.This value must be a multiple of 0x200. */
 
 gpio stm32gpio;
 
@@ -86,20 +113,73 @@ void ALL_leds_off()
 
 void SystemInit_1(void)
 {
-  // Enable clock for GPIOA and USB
-  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-  RCC->APB1ENR |= RCC_APB1ENR_USBEN;
+  /* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+  /* Set HSION bit */
+  RCC->CR |= (uint32_t)0x00000001;
+  /* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
+  RCC->CFGR &= (uint32_t)0xF8FF0000;
+  /* Reset HSEON, CSSON and PLLON bits */
+  RCC->CR &= (uint32_t)0xFEF6FFFF;
+  /* Reset HSEBYP bit */
+  RCC->CR &= (uint32_t)0xFFFBFFFF;
+  /* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
+  RCC->CFGR &= (uint32_t)0xFF80FFFF;
+  /* Disable all interrupts and clear pending bits  */
+  RCC->CIR = 0x009F0000;
+  /* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
+  /* Configure the Flash Latency cycles and enable prefetch buffer */
 
-  // Configure USB pull-up pin
-  GPIOA->CRH &= ~GPIO_CRH_MODE12;
-  GPIOA->CRH |= GPIO_CRH_MODE12_0;
+  __IO uint32_t StartUpCounter = 0, HSEStatus = 0;
 
-  // Initialize USB
-  NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
-  NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 1);
-  USB->CNTR = USB_CNTR_FRES; // Force reset
-  USB->CNTR = 0;             // Release reset
-  USB->ISTR = 0;             // Clear interrupt status register
+  /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/
+  /* Enable HSE */
+  RCC->CR |= ((uint32_t)RCC_CR_HSEON);
+  //  RCC->CR |= ((uint32_t)RCC_CR_HSION);
+
+  /* Wait till HSE is ready and if Time out is reached exit */
+  do
+  {
+    HSEStatus = RCC->CR & RCC_CR_HSERDY;
+    //    HSEStatus = RCC->CR & RCC_CR_HSIRDY;
+    StartUpCounter++;
+  } while (((RCC->CR & RCC_CR_HSERDY) == 0) && (StartUpCounter < HSE_STARTUP_TIMEOUT));
+
+  if ((RCC->CR & RCC_CR_HSERDY) != RESET) //	  if ((RCC->CR & RCC_CR_HSIRDY) != RESET)
+  {
+    // HSEStatus = (uint32_t)0x01;
+    /* Enable Prefetch Buffer */
+    FLASH->ACR |= FLASH_ACR_PRFTBE;
+    /* Flash 2 wait state */
+    FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+    FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+    /* HCLK = SYSCLK */
+    RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+    /* PCLK2 = HCLK */
+    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+    /* PCLK1 = HCLK */
+    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+
+    /*  PLL configuration: PLLCLK = HSE * 9 = 72 MHz */
+    RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
+    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSE | RCC_CFGR_PLLMULL9);
+    // PLLMULL9 - for 8MHz
+    //     RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSI_Div2 | RCC_CFGR_PLLMULL9);
+    // PLLMULL9 - for 8MHz
+    /* Enable PLL */
+    RCC->CR |= RCC_CR_PLLON;
+    /* Wait till PLL is ready */
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0)
+    {
+    } /* Select PLL as system clock source */
+    RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_SW));
+    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+    /* Wait till PLL is used as system clock source */
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)0x08)
+    {
+    }
+  }
+  SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET;
+  /* Vector Table Relocation in Internal FLASH. */
 }
 
 // void USB_LP_CAN1_RX0_IRQHandler(void)
@@ -114,6 +194,7 @@ void SystemInit_1(void)
 
 int main()
 {
+
   // SystemInit_1();
   gpio_init();
   ALL_leds_off();
